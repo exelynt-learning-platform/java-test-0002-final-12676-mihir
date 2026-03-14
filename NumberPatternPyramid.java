b2525b0e72e9d850c635d772f08c47f9da661a6f
mihirvaghasiya <mihirvaghasiya05@gmail.com>
2026-03-14 11:49:12 +0530

    test complete

---
+++ NumberPatternPyramid.java
@@ -0,0 +1,27 @@
+public class NumberPatternPyramid {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            // Print spaces
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+
+            // Increasing numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            // Decreasing numbers
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+}
