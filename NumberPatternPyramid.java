11f541c94090f776b457fd516a55de20beacd417
mihirvaghasiya <mihirvaghasiya05@gmail.com>
2026-03-23 13:46:57 +0530

    main to java-test-0002-12102 branch

--- NumberPatternPyramid.java
+++ NumberPatternPyramid.java
@@ -6,17 +6,14 @@ public class NumberPatternPyramid {
 
         for (int i = 1; i <= n; i++) {
 
-            // Print spaces
             for (int s = 1; s <= n - i; s++) {
                 System.out.print(" ");
             }
 
-            // Increasing numbers
             for (int j = 1; j <= i; j++) {
                 System.out.print(j);
             }
 
-            // Decreasing numbers
             for (int j = i - 1; j >= 1; j--) {
                 System.out.print(j);
             }
